1bb752992cf7bf56b2a0d27a6871f02ec08264f2
Mary Ann Burns <maburns@carolina.rr.com>
2016-08-07 14:38:02 -0400

    fizzbuzz

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	'use strict'
+
+function counter(integer) {
+
+  parsInt(integer,10);
+  
+  if (integer % 3);
+  return "fizz";
+  
+  else if (integer % 5);
+  return "buzz";
+  
+  else if (integer % 15);
+  return "fizzbuzz";
+  
+  else if (integer !== % 3,5,15);
+  return "integer";
+
+  
+}
